53f8b27be27188157661d27dac9ca8912df64cd0
Nikita <NikitaTropin689@yandex.ru>
2025-08-13 16:39:04 +0300

    Second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +57,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    implementation(libs.apollo.runtime)
+    implementation(libs.apollo.coroutines.support)
+
 }
--- app/src/main/java/com/example/gamification2/MainActivity.kt
+++ app/src/main/java/com/example/gamification2/MainActivity.kt
@@ -1,47 +1,73 @@
 package com.example.gamification2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.lifecycleScope
+import com.example.gamification2.network.WeatherRepository
+import com.example.gamification2.presentation.CurvedListScreen
 import com.example.gamification2.ui.theme.Gamification2Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Gamification2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+            val weatherRepository = WeatherRepository()
+
+            var text by remember { mutableStateOf("Loading") }
+
+            LaunchedEffect(true) {  // при старет приложения выполняется запрос
+                try {
+                    val weather = weatherRepository.fetchWeather(
+                        52.37125,
+                        4.89388
                     )
+                } catch (e: Exception) {
+                    e.localizedMessage ?: "error"
                 }
             }
+            Text(text = text, Modifier.padding(30.dp), fontSize = 16.sp)
+
+//            Gamification2Theme {
+////                CurvedListScreen()
+//
+//                Log.i("Begin", "Begin")
+//                var str: String? = "Something"
+//                lifecycleScope.launch {
+//                    try {
+//                        val weather = weatherRepository.fetchWeather(
+//                            52.37125,
+//                            4.89388
+//                        )
+//
+//                        str = weather!!.s
+//
+//                        Log.i("STR_WEATHER", str)
+//                    } catch (e: Exception) {
+//                        // Handle exception
+//                    }
+//
+//                }
+//
+//            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Gamification2Theme {
-        Greeting("Android")
     }
 }
